630fc74d9f2c0f8b4b9c098df23eb90b1283d764
andykim <andykim881028@gmail.com>
2019-06-07 18:44:30 +0900

    add scripts

---
+++ Assets/Scriptes/Component/System/MoveSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class MoveSystem : JobComponentSystem {
+    
+    [RequireComponentTag(typeof(Unit))]
+    [BurstCompile]
+    struct MoveSystemJob : IJobForEachWithEntity<Translation, MoveSpeed> {
+        [DeallocateOnJobCompletion] public NativeArray<Entity> units;
+        public void Execute(Entity entity, 
+                            int index, 
+                            ref Translation translation, 
+                            [ReadOnly] ref MoveSpeed moveSpeed) {
+            float distance = math.distance(translation.Value, Vector3.zero);
+            if (distance >= 10) {
+                translation.Value = 0;
+            }
+            translation.Value += moveSpeed.value;
+        }
+    }
+    private EndSimulationEntityCommandBufferSystem endSimulationEntityCommandBufferSystem;
+    protected override void OnCreate() {
+        this.endSimulationEntityCommandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        base.OnCreate();
+    }
+    protected override JobHandle OnUpdate(JobHandle inputDependencies) {
+        EntityQuery entityQuery = GetEntityQuery(typeof(Unit));
+        NativeArray<Entity> units = entityQuery.ToEntityArray(Allocator.TempJob);
+        MoveSystemJob moveJob = new MoveSystemJob {
+            units = units
+        };
+        JobHandle job = moveJob.Schedule(this, inputDependencies);
+        this.endSimulationEntityCommandBufferSystem.AddJobHandleForProducer(job);
+        return job;
+    }
+}
---
+++ Assets/Scriptes/IngameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class IngameManager : MonoBehaviour {
+    public Mesh mesh;
+    public Material material;
+
+    private void Start() {
+        Assert.IsNotNull(this.mesh);
+        Assert.IsNotNull(this.material);
+
+        EntityManager entitieManager = World.Active.EntityManager;
+        for (int i = 0; i < 1000; i++) {
+            SpawnEntity(entitieManager);
+        }
+    }
+
+    private void SpawnEntity(EntityManager entitieManager) {
+        Entity entity = CreateEntity(entitieManager);
+        SetEntityComponentData(entitieManager, 
+                               entity, 
+                               mesh, 
+                               material, 
+                               UnityEngine.Random.Range(0.1f, 0.5f), 
+                               UnityEngine.Random.Range(0, 10), 
+                               new float3(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(-0.5f, 0.5f), 0));
+    }
+   
+    private Entity CreateEntity(EntityManager entitieManager) {
+        Entity entity = entitieManager.CreateEntity(
+            typeof(Unit),
+            typeof(RenderMesh),
+            typeof(LocalToWorld),
+            typeof(Scale),
+            typeof(Translation),
+            typeof(MoveSpeed)
+        );
+        return entity;
+    }
+    private void SetEntityComponentData(EntityManager entitieManager,
+                                        Entity entity, 
+                                        Mesh mesh,
+                                        Material material,
+                                        float scale,
+                                        float3 position, 
+                                        float3 moveSpeed) {
+        entitieManager.SetSharedComponentData<RenderMesh>(entity, new RenderMesh {
+            mesh = mesh,
+            material = material,
+        });
+        entitieManager.SetComponentData<Scale>(entity, new Scale {
+            Value = scale,
+        });
+        entitieManager.SetComponentData<Translation>(entity, new Translation {
+            Value = position,
+        });
+        entitieManager.SetComponentData<MoveSpeed>(entity, new MoveSpeed {
+            value = moveSpeed,
+        });
+        
+    }
+}
